300f51cca91a3152f8f59ca692c5c9eae08ea15f
Poova156 <poovarasan.cs21@bitsathy.ac.in>
2022-11-16 16:05:39 +0530

    first commit

---
+++ lib/PickVideo.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapps/videoplayerfile.dart';
+import 'package:image_picker/image_picker.dart';
+
+class PickFile extends StatefulWidget {
+  PickFile({Key key}) : super(key: key);
+
+  @override
+  _PickFileState createState() => _PickFileState();
+}
+
+class _PickFileState extends State<PickFile> {
+  PickedFile _pickedFile;
+  final ImagePicker picker = ImagePicker();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () async {
+            _pickedFile = await picker.getVideo(source: ImageSource.gallery);
+
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (builder) => VideoPlayerFileCustum(
+                          videopath: _pickedFile.path,
+                        )));
+          },
+          child: Text("Upload video"),
+        ),
+      ),
+    );
+  }
+}
